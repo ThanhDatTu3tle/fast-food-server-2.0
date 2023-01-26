ca63d683ba0968cd0c4ff1287ae3ec3d0948cb81
ThanhDatTu3tle <thanhdat11badinh@gmail.com>
2023-01-26 19:31:49 +0700

    done API for Category & Product

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -3,8 +3,16 @@ import { ProductModel } from "../models/ProductModel.js";
 
 export const getCategory = async (req, res) => {
     try {
-        // const { categoryID } = req.value.params
         const category = await CategoryModel.find()
+        res.status(200).json(category)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    } 
+};
+
+export const getOneCategory = async (req, res) => {
+    try {
+        const category = await CategoryModel.findById(req.params.id).populate("products")
         res.status(200).json(category)
     } catch (error) {
         res.status(500).json({ error: error })
@@ -16,6 +24,29 @@ export const createCategory = async (req, res) => {
         const newCategory = new CategoryModel(req.body)
         const savedCategory = await newCategory.save()
         res.status(200).json(savedCategory)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    } 
+};
+
+export const updateCategory = async (req, res) => {
+    try {
+        const category = await CategoryModel.findById(req.params.id)
+        await category.updateOne({ $set: req.body })
+        res.status(200).json("Updated successfully!!!")
+    } catch (error) {
+        res.status(500).json({ error: error })
+    } 
+};
+
+export const deleteCategory = async (req, res) => {
+    try {
+        await ProductModel.updateMany(
+            { category: req.params.id }, 
+            { category: null }
+        )
+        await CategoryModel.findByIdAndDelete(req.params.id)
+        res.status(200).json("Deleted successfully!!!")
     } catch (error) {
         res.status(500).json({ error: error })
     } 
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,8 +3,16 @@ import { ProductModel } from "../models/ProductModel.js";
 
 export const getProduct = async (req, res) => {
     try {
-        // const { categoryID } = req.value.params
         const product = await ProductModel.find()
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    } 
+};
+
+export const getOneProduct = async (req, res) => {
+    try {
+        const product = await ProductModel.findById(req.params.id)
         res.status(200).json(product)
     } catch (error) {
         res.status(500).json({ error: error })
@@ -20,6 +28,29 @@ export const createProduct = async (req, res) => {
             await category.updateOne({ $push: { products: savedProduct._id } })
         }
         res.status(200).json(savedProduct)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    } 
+};
+
+export const updateProduct = async (req, res) => {
+    try {
+        const product = await ProductModel.findById(req.params.id)
+        await product.updateOne({ $set: req.body })
+        res.status(200).json("Updated successfully!!!")
+    } catch (error) {
+        res.status(500).json({ error: error })
+    } 
+};
+
+export const deleteProduct = async (req, res) => {
+    try {
+        await CategoryModel.updateMany(
+            { products: req.params.id }, 
+            { $pull: { products: req.params.id } }
+        )
+        await ProductModel.findByIdAndDelete(req.params.id)
+        res.status(200).json("Deleted successfully!!!")
     } catch (error) {
         res.status(500).json({ error: error })
     } 
